4e07d935c22d81c851dcf1ad1dbabc77870bb26c
bahrus <anderson.bruce.b@gmail.com>
2018-12-09 10:01:53 -0500

    Working

---
+++ h2h-tf.js
@@ -0,0 +1,84 @@
+import { define } from 'xtal-latx/define.js';
+import { XtallatX, disabled } from 'xtal-latx/xtal-latx.js';
+export class H2H_TF extends XtallatX(HTMLElement) {
+    constructor() {
+        super(...arguments);
+        this._getTarget = t => {
+            let candidate = t.previousElementSibling;
+            if (!candidate)
+                candidate = t.parentElement;
+            return candidate;
+        };
+    }
+    static get is() {
+        return 'h2h-tf';
+    }
+    static get observedAttributes() {
+        return [disabled];
+    }
+    get transform() {
+        return this._transform;
+    }
+    set transform(nv) {
+        this._transform = nv;
+        this.onPropsChange();
+    }
+    get getTarget() {
+        return this._getTarget;
+    }
+    set getTarget(nv) {
+        this._getTarget = nv;
+    }
+    connectedCallback() {
+        this.style.display = 'none';
+        this._upgradeProperties([disabled, 'transform', 'target']);
+        this._c = true;
+        this.onPropsChange();
+    }
+    onPropsChange() {
+        if (this._disabled || !this._c || !this._transform)
+            return;
+        const target = this._getTarget(this);
+        if (target === null) {
+            setTimeout(() => {
+                this.onPropsChange();
+            }, 50);
+            return;
+        }
+        const div = document.createElement('div');
+        this.value = div;
+        const context = {
+            rootEl: this.value,
+            stack: [],
+            leaf: this.value,
+            processChildren: false,
+            el: null,
+        };
+        this.process(target, context);
+        this.de('value', {
+            value: this.value
+        });
+    }
+    process(target, context) {
+        for (const selector in this._transform) {
+            if (target.matches && target.matches(selector)) {
+                const transformTemplate = this._transform[selector];
+                context.processChildren = false;
+                context.el = target;
+                transformTemplate(context);
+                if (context.processChildren && target.childNodes) {
+                    target.childNodes.forEach(node => {
+                        if (!node.matches)
+                            return;
+                        this.process(node, context);
+                    });
+                    const s = context.stack;
+                    s.pop();
+                    context.leaf = s.length > 0 ? s[s.length - 1] : null;
+                }
+            }
+        }
+    }
+}
+define(H2H_TF);
+//# sourceMappingURL=h2h-tf.js.map
---
+++ h2h-tf.ts
@@ -0,0 +1,96 @@
+import {define} from 'xtal-latx/define.js';
+import {XtallatX, disabled} from 'xtal-latx/xtal-latx.js';
+
+export interface IContext{
+    rootEl: any,
+    stack: any[],
+    processChildren: boolean;
+    leaf: any,
+    el: Element | null,
+}
+
+export class H2H_TF extends XtallatX(HTMLElement){
+    static get is(){
+        return 'h2h-tf';
+    }
+    static get observedAttributes(){
+        return [disabled];
+    }
+    _transform! : {[key: string] : (context: IContext) => void }
+
+    get transform(){
+        return this._transform;
+    }
+    set transform(nv){
+        this._transform = nv;
+        this.onPropsChange();
+    }
+
+    _getTarget : (h: H2H_TF) => Element | null = t => {
+        let candidate = t.previousElementSibling;
+        if(!candidate) candidate = t.parentElement
+        return candidate;
+    }
+    get getTarget(){
+        return this._getTarget;
+    }
+    set getTarget(nv){
+        this._getTarget = nv;
+    }
+
+
+    _c!: boolean;
+    connectedCallback(){
+        this.style.display = 'none';
+        this._upgradeProperties([disabled, 'transform', 'target']);
+        this._c = true;
+        this.onPropsChange();
+    }
+    value!: any;
+    onPropsChange(){
+        if(this._disabled || !this._c || !this._transform) return;
+        const target = this._getTarget(this);
+        if(target === null){
+            setTimeout(() =>{
+                this.onPropsChange()
+            }, 50);
+            return;
+        }
+        const div = document.createElement('div');
+        this.value = div;
+        const context  = {
+            rootEl: this.value, 
+            stack: [],
+            leaf: this.value,
+            processChildren: false,
+            el: null,
+        } as IContext;
+        
+        this.process(target, context);
+        this.de('value', {
+            value: this.value
+        })
+    }
+
+    process(target: Element, context: IContext){
+        for(const selector in this._transform){
+            if(target.matches && target.matches(selector)){
+                const transformTemplate = this._transform[selector];
+                context.processChildren = false;
+                context.el = target;
+                transformTemplate(context);
+                if(context.processChildren && target.childNodes){
+                    target.childNodes.forEach(node =>{
+                        if(!(<any>node).matches) return;
+                        this.process((<any>node) as Element, context)
+                    });
+                    const s = context.stack;
+                    s.pop();
+                    context.leaf = s.length > 0 ? s[s.length -1] : null;
+                }
+            }
+        }
+    }
+}
+
+define(H2H_TF)
